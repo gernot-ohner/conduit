919d8527c84dec60374ef28b72c3399f0967ddd7
Gernot Ohner <gernot.ohner@locatee.ch>
2023-08-14 21:23:07 +0200

    Add testcontainers

--- src/test/java/dev/ohner/conduit/ConduitApplicationTests.java
+++ src/test/java/dev/ohner/conduit/ConduitApplicationTests.java
@@ -2,9 +2,18 @@ package dev.ohner.conduit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class ConduitApplicationTests {
+
+	@Container
+	@ServiceConnection
+	static PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:latest");
 
 	@Test
 	void contextLoads() {
